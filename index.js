0916772c68300868cee319c1a8f579ae54da4ef9
kelly-brightroot <ksharp@brightroot.com>
2025-03-04 16:53:46 -0700

    rer

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const functions = require('@google-cloud/functions-framework');
 functions.http('helloHttp', (req, res) => {
   res.send(`Hello ${req.query.name || req.body.name || 'World'}!`);
 });
-
+ 
